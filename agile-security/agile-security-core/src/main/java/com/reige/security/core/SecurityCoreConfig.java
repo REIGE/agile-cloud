db243e7b29d607f4df710524badcfda7ca2a977e
reige <407601459@qq.com>
2018-08-17 00:02:13 +0800

    Initial commit

---
+++ agile-security/agile-security-core/src/main/java/com/reige/security/core/SecurityCoreConfig.java
@@ -0,0 +1,11 @@
+package com.reige.security.core;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author chenrui
+ * @date 2018/8/16.
+ */
+@Configuration
+public class SecurityCoreConfig {
+}
